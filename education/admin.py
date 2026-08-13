c2a47b7a4a519faa5a1cf104fde6726b551adec5
Ahmadjon-Ergashev <111.ahmadjon@gmail.com>
2026-08-13 16:46:30 +0500

    feat: register education models with TranslatableAdmin for multilingual support

--- education/admin.py
+++ education/admin.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
+from parler.admin import TranslatableAdmin
 
 from .models import Country, EducationForm, EducationLanguage, EducationType, Specialty, University
 # Register your models here.
 
-admin.site.register(Country)
-admin.site.register(EducationForm)
-admin.site.register(EducationLanguage)
-admin.site.register(EducationType)
-admin.site.register(Specialty)
-admin.site.register(University)
+admin.site.register(Country, TranslatableAdmin)
+admin.site.register(EducationForm, TranslatableAdmin)
+admin.site.register(EducationLanguage, TranslatableAdmin)
+admin.site.register(EducationType, TranslatableAdmin)
+admin.site.register(Specialty, TranslatableAdmin)
+admin.site.register(University, TranslatableAdmin)
