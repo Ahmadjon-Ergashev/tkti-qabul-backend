c385482e8cc1b60544b6bba54c355eb4236a814a
Ahmadjon-Ergashev <111.ahmadjon@gmail.com>
2026-08-20 17:50:22 +0500

    feat: add specialty_id field to ApplicationSerializer for improved data handling

--- application/serializers.py
+++ application/serializers.py
@@ -2,13 +2,15 @@ from rest_framework import serializers
 
 from user.serializers import StudentSerializer
 from .models import Application
+from education.models import Specialty
 
 
 class ApplicationSerializer(serializers.ModelSerializer):
     student = StudentSerializer(read_only=True)
     specialty = serializers.StringRelatedField(read_only=True)
+    specialty_id = serializers.PrimaryKeyRelatedField(queryset=Specialty.objects.all(), source='specialty', write_only=True)
 
     class Meta:
         model = Application
-        fields = ['id', 'student', 'specialty', 'invoice', 'description', 'is_approved', 'application_date', 'updated_at']
+        fields = ['id', 'student', 'specialty', 'invoice', 'description', 'is_approved', 'application_date', 'updated_at', 'specialty_id']
         read_only_fields = ['application_date', 'updated_at', 'is_approved', 'student']
